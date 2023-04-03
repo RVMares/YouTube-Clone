a8a28af698aa48b76c0c0b674edf3e4d12afb533
Ruth Hermosillo <hermosilloruth59@gmail.com>
2023-04-03 12:14:00 -0600

    Add get_video_comments Function and Modify Urls Files

--- backend/youtube_clone_comment/views.py
+++ backend/youtube_clone_comment/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.decorators import api_view, permission_classes
+from .models import Comment
+from .serializers import CommentSerializer
+from django.shortcuts import get_object_or_404
 
-# Create your views here.
+@api_view(['GET'])
+@permission_classes([AllowAny])
+def get_video_comments(request, pk):
+
+    comments = get_object_or_404(Comment, pk=pk)
+
+    serialized_comments = CommentSerializer(comments, many=True)
+
+    return Response(serialized_comments.data, status=status.HTTP_200_OK)
